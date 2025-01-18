bca066eae57f6b9f7561b69d4c2ddf6ec3dfbb6f
Alexandr Melnichenko <alexandr.melnichenko@gmail.com>
2025-01-18 10:46:32 +0200

    ready hw-2

---
+++ src/server.js
@@ -0,0 +1,68 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+import { getAllContacts, getContactById } from './services/contacts';
+
+const PORT = 3000;
+
+// Number(getEnvVar('PORT', '3000'));
+
+export const setupServer = () => {
+  const app = express();
+
+  app.use(express.json());
+  app.use(cors());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Mongo connection successfully established!',
+    });
+  });
+
+  app.get('/contacts', async (req, res) => {
+    const contacts = await getAllContacts();
+
+    res.status(200).json({
+      data: contacts,
+    });
+  });
+
+  app.get('/contacts/:contactId', async (req, res, next) => {
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);
+
+    if (!contact) {
+      res.status(404).json({
+        message: 'Contact not found',
+      });
+      return;
+    }
+
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully found contact with id {contactId}!',
+      data: {
+        // об'єкт контакту
+      },
+    });
+  });
+
+  app.use('*', (req, res, next) => {
+    res.status(404).json({
+      message: 'Contact not found',
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
