50bab13b5a7f918f3c3d48bc69a0f61d4bb0b42f
Alexandr Melnichenko <alexandr.melnichenko@gmail.com>
2025-01-18 12:09:22 +0200

    fix import

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { initMongoConnection } from './db/initMongoConnection';
+import { initMongoConnection } from './db/initMongoConnection.js';
 import { setupServer } from './server';
 
 const bootstrap = async () => {
