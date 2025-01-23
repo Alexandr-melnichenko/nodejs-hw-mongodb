4a87b2dd7b1525358553f9361532b97d321b663d
Alexandr Melnichenko <alexandr.melnichenko@gmail.com>
2025-01-23 23:04:14 +0200

    fix import deleteContact

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -3,6 +3,7 @@ import {
   getContactById,
   createContact,
   updateContact,
+  deleteContact,
 } from '../services/contacts.js';
 import createHttpError from 'http-errors';
 
@@ -64,29 +65,3 @@ export const deleteContactController = async (req, res, next) => {
 
   res.status(204).send();
 };
-
-// app.get('/contacts', async (req, res) => {
-//   const contacts = await getAllContacts();
-
-//   res.status(200).json({
-//     data: contacts,
-//   });
-// });
-
-// app.get('/contacts/:contactId', async (req, res, next) => {
-//   const { contactId } = req.params;
-//   const contact = await getContactById(contactId);
-
-//   if (!contact) {
-//     res.status(404).json({
-//       message: 'Contact not found',
-//     });
-//     return;
-//   }
-
-//   res.status(200).json({
-//     status: 200,
-//     message: 'Successfully found contact with id {contactId}!',
-//     data: contact,
-//   });
-// });
