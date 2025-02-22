6fb7767fc4b77426f962587d00f741a69a260000
Alexandr Melnichenko <alexandr.melnichenko@gmail.com>
2025-02-22 17:18:13 +0200

    fix nullable

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -12,7 +12,7 @@ export const updateContactSchema = Joi.object({
   name: Joi.string().min(3).max(20),
   phoneNumber: Joi.string().min(3).max(20),
   email: Joi.string().email(),
-  isFavourite: Joi.boolean(),
+  isFavourite: Joi.boolean().truthy('true').falsy('false').default(false),
   contactType: Joi.string().valid('work', 'home', 'personal'),
 });
 
